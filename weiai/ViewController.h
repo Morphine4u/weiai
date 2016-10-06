a4c428bf099c5841ed92530b2810aa5630086bba
Morphine <morphine@morphinedembp.lan>
2016-10-07 00:19:50 +0800

    Initial Commit

---
+++ weiai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiai
+//
+//  Created by Morphine on 2016/10/7.
+//  Copyright © 2016年 Morphine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weiai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weiai
+//
+//  Created by Morphine on 2016/10/7.
+//  Copyright © 2016年 Morphine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
